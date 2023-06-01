381a79a2bb605a71a0fa3db776246a21b60de739
Shanavas Shaji <shanavas.shaji@basemark.com>
2023-06-01 05:42:43 +0300

    Added tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(TikTokApp());
@@ -7,81 +8,116 @@ void main() {
 class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Set the status bar color to black
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+    ));
     return MaterialApp(
       title: 'TikTok',
       theme: ThemeData(
         primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: CustomPageTransitionsBuilder(),
+            TargetPlatform.iOS: CustomPageTransitionsBuilder(),
+          },
+        ),
       ),
       home: HomePage(),
     );
   }
 }
 
+class CustomPageTransitionsBuilder extends PageTransitionsBuilder {
+  @override
+  Widget buildTransitions<T>(PageRoute<T> route, BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    if (route.isFirst) {
+      return child;
+    }
+    // Customize your transition animation here
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2, // Number of tabs
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('TikTok'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'Tab 1'),
-              Tab(text: 'Tab 2'),
-            ],
+        body: SafeArea(
+          child: Container(
+            color: Colors.black,
+            child: Column(
+              children: [
+                TabBar(
+                  tabs: [
+                    Tab(text: 'Tab 1'),
+                    Tab(text: 'Tab 2'),
+                  ],
+                ),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      // Contents of Tab 1
+                      Container(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: MediaQuery.of(context).size.height,
+                                child: PageView.builder(
+                                  scrollDirection: Axis.vertical,
+                                  itemCount: 10,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    return VideoFeed(
+                                      videoUrl: 'https://example.com/video.mp4', // Replace with video URL
+                                      username: 'User $index',
+                                      caption: 'This is video $index',
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      // Contents of Tab 2
+                      Container(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: MediaQuery.of(context).size.height,
+                                child: PageView.builder(
+                                  scrollDirection: Axis.vertical,
+                                  itemCount: 10,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    return VideoFeed(
+                                      videoUrl: 'https://example.com/video.mp4', // Replace with video URL
+                                      username: 'User $index',
+                                      caption: 'This is video $index',
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        body: TabBarView(
-          children: [
-            // Contents of Tab 1
-            Container(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height,
-                      child: PageView.builder(
-                        scrollDirection: Axis.vertical,
-                        itemCount: 10,
-                        itemBuilder: (BuildContext context, int index) {
-                          return VideoFeed(
-                            videoUrl: 'https://example.com/video.mp4', // Replace with video URL
-                            username: 'User $index',
-                            caption: 'This is video $index',
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            // Contents of Tab 2
-            Container(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height,
-                      child: PageView.builder(
-                        scrollDirection: Axis.vertical,
-                        itemCount: 10,
-                        itemBuilder: (BuildContext context, int index) {
-                          return VideoFeed(
-                            videoUrl: 'https://example.com/video.mp4', // Replace with video URL
-                            username: 'User $index',
-                            caption: 'This is video $index',
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
         ),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -118,31 +154,36 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Discover',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: 'Create',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              label: 'Inbox',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          selectedItemColor: Colors.pink,
-          unselectedItemColor: Colors.grey,
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.black, // Set the background color to black
+          ),
+          child: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Discover',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add),
+                label: 'Activity',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark),
+                label: 'Bookmarks',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.grey,
+          ),
         ),
       ),
     );
@@ -170,17 +211,20 @@ class VideoFeed extends StatelessWidget {
         fit: StackFit.expand,
         children: [
           VideoPlayerWidget(videoUrl: videoUrl),
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.grey,
-              radius: 20,
-              child: Text(
-                username.substring(0, 1).toUpperCase(),
-                style: TextStyle(fontSize: 18),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey,
+                radius: 20,
+                child: Text(
+                  username.substring(0, 1).toUpperCase(),
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
+              title: Text(username),
+              subtitle: Text(caption),
             ),
-            title: Text(username),
-            subtitle: Text(caption),
           ),
         ],
       ),
