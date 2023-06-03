893ed692954491e809efc5ce53b95a6becf0ed4c
Shanavas Shaji <shanavas.shaji@basemark.com>
2023-06-03 18:58:50 +0300

    Added logic to hide unwanted container

--- lib/main.dart
+++ lib/main.dart
@@ -411,6 +411,12 @@ class FlashCardFeedState extends State<FlashCardFeed> {
   }
 
   bool showBackOfFlashCard = false;
+  bool showButtonOne = true;
+  bool showButtonTwo = true;
+  bool showButtonThree = true;
+  bool showButtonFour = true;
+  bool showButtonFive = true;
+  bool isColoredBoxSelected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -462,10 +468,15 @@ class FlashCardFeedState extends State<FlashCardFeed> {
               setState(() {
                 print("inside set state");
                 showBackOfFlashCard = !showBackOfFlashCard;
+                showButtonOne = true;
+                showButtonTwo = true;
+                showButtonThree = true;
+                showButtonFour = true;
+                showButtonFive = true;
+                isColoredBoxSelected = false;
               });
             },
             child: Container(
-              color: Colors.blue,
               width: double.infinity,
               height: double.infinity,
               child: Padding(
@@ -539,101 +550,208 @@ class FlashCardFeedState extends State<FlashCardFeed> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                      color: princetonOrange,
-                                    ),
-                                    width: (MediaQuery.of(context).size.width - 8 * 6) / 5,
-                                    height: 52,
-                                    child: const Center(
-                                      child: Text(
-                                        "1",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.w600,
+                                Visibility(
+                                  visible: showButtonOne,
+                                  child: Expanded(
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          if(!isColoredBoxSelected) {
+                                            showButtonTwo = false;
+                                            showButtonThree = false;
+                                            showButtonFour = false;
+                                            showButtonFive = false;
+                                            isColoredBoxSelected = true;
+                                          } else {
+                                            showBackOfFlashCard = false;
+                                          }
+                                        });
+                                      },
+                                      behavior: HitTestBehavior.translucent,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(8),
+                                          color: princetonOrange,
+                                        ),
+                                        width: (MediaQuery.of(context).size.width - 8 * 6) / 5,
+                                        height: 52,
+                                        child: const Center(
+                                          child: Text(
+                                            "1",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
                                 ),
-                                SizedBox(width: 8),
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                      color: mellowApricot,
-                                    ),
-                                    height: 52,
-                                    child: const Center(
-                                      child: Text(
-                                        "2",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.w600,
+                                Visibility(
+                                    visible: showButtonOne,
+                                    child: SizedBox(width: 8)
+                                ),
+                                Visibility(
+                                  visible: showButtonTwo,
+                                  child: Expanded(
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          if(!isColoredBoxSelected) {
+                                            showButtonOne = false;
+                                            showButtonThree = false;
+                                            showButtonFour = false;
+                                            showButtonFive = false;
+                                            isColoredBoxSelected = true;
+                                          } else {
+                                            showBackOfFlashCard = false;
+                                          }
+                                        });
+                                      },
+                                      behavior: HitTestBehavior.translucent,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(8),
+                                          color: mellowApricot,
+                                        ),
+                                        height: 52,
+                                        child: const Center(
+                                          child: Text(
+                                            "2",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
                                 ),
-                                SizedBox(width: 8),
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                      color: mustard,
-                                    ),
-                                    height: 52,
-                                    child: const Center(
-                                      child: Text(
-                                        "3",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.w600,
+                                Visibility(
+                                    visible: showButtonTwo,
+                                    child: SizedBox(width: 8)
+                                ),
+                                Visibility(
+                                  visible: showButtonThree,
+                                  child: Expanded(
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          if(!isColoredBoxSelected) {
+                                            showButtonOne = false;
+                                            showButtonTwo = false;
+                                            showButtonFour = false;
+                                            showButtonFive = false;
+                                            isColoredBoxSelected = true;
+                                          } else {
+                                            showBackOfFlashCard = false;
+                                          }
+                                        });
+                                      },
+                                      behavior: HitTestBehavior.translucent,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(8),
+                                          color: mustard,
+                                        ),
+                                        height: 52,
+                                        child: const Center(
+                                          child: Text(
+                                            "3",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
                                 ),
-                                SizedBox(width: 8),
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                      color: darkGreenColor,
-                                    ),
-                                    height: 52,
-                                    child: const Center(
-                                      child: Text(
-                                        "4",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.w600,
+                                Visibility(
+                                    visible: showButtonThree,
+                                    child: SizedBox(width: 8)
+                                ),
+                                Visibility(
+                                  visible: showButtonFour,
+                                  child: Expanded(
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          if(!isColoredBoxSelected) {
+                                            showButtonOne = false;
+                                            showButtonTwo = false;
+                                            showButtonThree = false;
+                                            showButtonFive = false;
+                                            isColoredBoxSelected = true;
+                                          } else {
+                                            showBackOfFlashCard = false;
+                                          }
+                                        });
+                                      },
+                                      behavior: HitTestBehavior.translucent,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(8),
+                                          color: darkGreenColor,
+                                        ),
+                                        height: 52,
+                                        child: const Center(
+                                          child: Text(
+                                            "4",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
                                   ),
                                 ),
-                                SizedBox(width: 8),
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(8),
-                                      color: illuminatingEmerald,
-                                    ),
-                                    height: 52,
-                                    child: const Center(
-                                      child: Text(
-                                        "5",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.w600,
+                                Visibility(
+                                    visible: showButtonOne,
+                                    child: SizedBox(width: 8)
+                                ),
+                                Visibility(
+                                  visible: showButtonFive,
+                                  child: Expanded(
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          if(!isColoredBoxSelected) {
+                                            showButtonOne = false;
+                                            showButtonTwo = false;
+                                            showButtonThree = false;
+                                            showButtonFour = false;
+                                            isColoredBoxSelected = true;
+                                          } else {
+                                            showBackOfFlashCard = false;
+                                          }
+                                        });
+                                      },
+                                      behavior: HitTestBehavior.translucent,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(8),
+                                          color: illuminatingEmerald,
+                                        ),
+                                        height: 52,
+                                        child: const Center(
+                                          child: Text(
+                                            "5",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ),
