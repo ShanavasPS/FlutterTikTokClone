419f2e3dd54515774b00149df71fbf3fe65ba400
Shanavas Shaji <shanavas.shaji@basemark.com>
2023-06-06 15:38:40 +0300

    Optimised network calls logic

--- lib/network/network_calls.dart
+++ lib/network/network_calls.dart
@@ -1,41 +1,27 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<Map<String, dynamic>> getNextFollowingItem() async {
-  final response = await http.get(Uri.parse('https://cross-platform.rp.devfactory.com/following'));
-  print('inside getNextFollowingItem:');
+Future<Map<String, dynamic>> fetchData(String url) async {
+  final response = await http.get(Uri.parse(url));
+  print('Inside fetchData:');
   if (response.statusCode == 200) {
-    print("received positive response");
+    print("Received positive response");
     print(response.body);
     var data = jsonDecode(response.body);
     return data;
   } else {
-    throw Exception('Failed to fetch next item');
+    throw Exception('Failed to fetch data');
   }
+}
+
+Future<Map<String, dynamic>> getNextFollowingItem() async {
+  return fetchData('https://cross-platform.rp.devfactory.com/following');
 }
 
 Future<Map<String, dynamic>> getNextForYouItem() async {
-  final response = await http.get(Uri.parse('https://cross-platform.rp.devfactory.com/for_you'));
-  print('inside getNextForYouItem:');
-  if (response.statusCode == 200) {
-    print("received positive response");
-    print(response.body);
-    var data = jsonDecode(response.body);
-    return data;
-  } else {
-    throw Exception('Failed to fetch next item');
-  }
+  return fetchData('https://cross-platform.rp.devfactory.com/for_you');
 }
 
 Future<Map<String, dynamic>> revealAnswer(int mcqID) async {
-  final response = await http.get(Uri.parse('https://cross-platform.rp.devfactory.com/reveal?id=$mcqID'));
-  print('inside revealAnswer:');
-  if (response.statusCode == 200) {
-    print("received positive response");
-    print(response.body);
-    var data = jsonDecode(response.body);
-    return data;
-  } else {
-    throw Exception('Failed to fetch next item');
-  }
+  return fetchData('https://cross-platform.rp.devfactory.com/reveal?id=$mcqID');
 }
