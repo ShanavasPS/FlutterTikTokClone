ecefb042ca15c45ed4c60200bdbe4dfc55cd6b14
Shanavas Shaji <shanavas.shaji@basemark.com>
2023-06-07 08:44:32 +0300

    Further optimized rating view

--- lib/views/rating_view.dart
+++ lib/views/rating_view.dart
@@ -8,7 +8,9 @@ import '../widgets/rating_button.dart';
 class RatingView extends StatefulWidget {
   final bool showBackOfFlashCard;
   final Function(bool) updateFlashCardBackState;
-  const RatingView({super.key, required this.showBackOfFlashCard, required this.updateFlashCardBackState});
+
+  const RatingView({Key? key, required this.showBackOfFlashCard, required this.updateFlashCardBackState})
+      : super(key: key);
 
   @override
   RatingViewState createState() => RatingViewState();
@@ -23,24 +25,27 @@ class RatingViewState extends State<RatingView> {
     print('inside state:');
   }
 
-  bool isColoredBoxSelected = false;
-
-  bool didTapOnButtonOne = false;
-  bool didTapOnButtonTwo = false;
-  bool didTapOnButtonThree = false;
-  bool didTapOnButtonFour = false;
-  bool didTapOnButtonFive = false;
+  List<bool> didTapOnButtons = [false, false, false, false, false];
 
   @override
   Widget build(BuildContext context) {
+    bool didTapOnAnyButton = didTapOnButtons.any((tapped) => tapped);
 
-    bool didTapOnAnyButton = didTapOnButtonOne || didTapOnButtonTwo || didTapOnButtonThree || didTapOnButtonFour || didTapOnButtonFive;
+    List<Color> ratingColors = [
+      TikTokColors.princetonOrange,
+      TikTokColors.mellowApricot,
+      TikTokColors.mustard,
+      TikTokColors.darkGreenColor,
+      TikTokColors.illuminatingEmerald,
+    ];
 
-    bool showButtonOne = !didTapOnAnyButton || didTapOnButtonOne;
-    bool showButtonTwo = !didTapOnAnyButton || didTapOnButtonTwo;
-    bool showButtonThree = !didTapOnAnyButton || didTapOnButtonThree;
-    bool showButtonFour = !didTapOnAnyButton || didTapOnButtonFour;
-    bool showButtonFive = !didTapOnAnyButton || didTapOnButtonFive;
+    List<String> ratingStrings = [
+      TikTokStrings.ratingOne,
+      TikTokStrings.ratingTwo,
+      TikTokStrings.ratingThree,
+      TikTokStrings.ratingFour,
+      TikTokStrings.ratingFive,
+    ];
 
     return Column(
       children: [
@@ -61,112 +66,26 @@ class RatingViewState extends State<RatingView> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Visibility(
-              visible: showButtonOne,
-              child: Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if(!didTapOnAnyButton) {
-                        didTapOnButtonOne = true;
-                      } else {
-                        widget.updateFlashCardBackState(false);
-                      }
-                    });
-                  },
-                  behavior: HitTestBehavior.translucent,
-                  child: ratingButton(context, TikTokStrings.ratingOne, TikTokColors.princetonOrange),
+            for (int i = 0; i < didTapOnButtons.length; i++) ...[
+              if (!didTapOnAnyButton || didTapOnButtons[i]) ...[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (!didTapOnAnyButton) {
+                          didTapOnButtons[i] = true;
+                        } else {
+                          widget.updateFlashCardBackState(false);
+                        }
+                      });
+                    },
+                    behavior: HitTestBehavior.translucent,
+                    child: ratingButton(context, ratingStrings[i], ratingColors[i]),
+                  ),
                 ),
-              ),
-            ),
-            Visibility(
-                visible: showButtonOne,
-                child: const SizedBox(width: 8)
-            ),
-            Visibility(
-              visible:showButtonTwo,
-              child: Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if(!didTapOnAnyButton) {
-                        didTapOnButtonTwo = true;
-                      } else {
-                        widget.updateFlashCardBackState(false);
-                      }
-                    });
-                  },
-                  behavior: HitTestBehavior.translucent,
-                  child: ratingButton(context, TikTokStrings.ratingTwo, TikTokColors.mellowApricot),
-                ),
-              ),
-            ),
-            Visibility(
-                visible: showButtonTwo,
-                child: const SizedBox(width: 8)
-            ),
-            Visibility(
-              visible: showButtonThree,
-              child: Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if(!didTapOnAnyButton) {
-                        didTapOnButtonThree = true;
-                      } else {
-                        widget.updateFlashCardBackState(false);
-                      }
-                    });
-                  },
-                  behavior: HitTestBehavior.translucent,
-                  child: ratingButton(context, TikTokStrings.ratingThree, TikTokColors.mustard),
-                ),
-              ),
-            ),
-            Visibility(
-                visible: showButtonThree,
-                child: const SizedBox(width: 8)
-            ),
-            Visibility(
-              visible: showButtonFour,
-              child: Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if(!didTapOnAnyButton) {
-                        didTapOnButtonFour = true;
-                      } else {
-                        widget.updateFlashCardBackState(false);
-                      }
-                    });
-                  },
-                  behavior: HitTestBehavior.translucent,
-                  child: ratingButton(context, TikTokStrings.ratingFour, TikTokColors.darkGreenColor),
-                ),
-              ),
-            ),
-            Visibility(
-                visible: showButtonFour,
-                child: const SizedBox(width: 8)
-            ),
-            Visibility(
-              visible: showButtonFive,
-              child: Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if(!didTapOnAnyButton) {
-                        didTapOnButtonFive = true;
-                      } else {
-                        widget.updateFlashCardBackState(false);
-                      }
-                    });
-                  },
-                  behavior: HitTestBehavior.translucent,
-                  child: ratingButton(context, TikTokStrings.ratingFive, TikTokColors.illuminatingEmerald),
-                ),
-              ),
-            ),
+                if (i < didTapOnButtons.length - 1) const SizedBox(width: 8),
+              ],
+            ],
           ],
         ),
       ],
